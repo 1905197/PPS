d7603a194ade95176c352ef28c0ded7c203d6b82
1905197 <60253218+1905197@users.noreply.github.com>
2020-02-07 10:31:56 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,10 @@
+/*Program to write N number time "I will switch off lights, fans and Ac when leaving room".*/
+#include <stdio.h>
+int main()
+{
+int a;
+printf("Enter the Number");
+scanf("%d",&a);
+for (int i =1; i<= a; i++)
+printf("\nI will switch off lights, fans and Ac when leaving room");
+}
